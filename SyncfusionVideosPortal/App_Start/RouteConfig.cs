bb718554304ad9ac4f6dee5de7e44658ca14fa5d
ROOT\vijayakumar.yoganand <vijayakumar.yoganandan@syncfusion.com>
2018-12-27 19:15:29 +0530

    Route config changes

--- SyncfusionVideosPortal/App_Start/RouteConfig.cs
+++ SyncfusionVideosPortal/App_Start/RouteConfig.cs
@@ -26,6 +26,12 @@ namespace SyncfusionVideosPortal
             );
 
             routes.MapRoute(
+               name: "VideoDetailsPage",
+               url: "videos/{platform}/{slugTitle}",
+               defaults: new { controller = "Platform", action = "VideoDetailsPage" }
+           );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
